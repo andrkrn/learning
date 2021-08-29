589839532ee71d3e6d9a646e84274667bf22578b
Andri Kurnia <andrikurnia@live.com>
2021-08-29 21:27:21 +0700

    hello world final

--- go/1-hello-world/hello.go
+++ go/1-hello-world/hello.go
@@ -2,10 +2,26 @@ package main
 
 import "fmt"
 
+const helloEn = "Hello, "
+const helloId = "Hai, "
+
 func main() {
-	fmt.Println(Hello("world!"))
+	fmt.Println(Hello("world!", ""))
 }
 
-func Hello(p string) string {
-	return "Hello, " + p
+func Hello(n string, lang string) string {
+	if n == "" {
+		n = "World"
+	}
+
+	return Greeting(lang) + n
+}
+
+func Greeting(lang string) string {
+	switch lang {
+	case "id":
+		return helloId
+	default:
+		return helloEn
+	}
 }
--- go/1-hello-world/hello_test.go
+++ go/1-hello-world/hello_test.go
@@ -3,10 +3,32 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("world!")
-	want := "Hello, world!"
+	assertCorrectMessage := func(t *testing.T, got string, want string) {
+		t.Helper()
 
-	if got != want {
-		t.Errorf("got %q wamt %q", got, want)
+		if got != want {
+			t.Errorf("got %q wamt %q", got, want)
+		}
 	}
+
+	t.Run("say hello", func(t *testing.T) {
+		got := Hello("world!", "")
+		want := "Hello, world!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("default to hello world", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say hello in indonesia", func(t *testing.T) {
+		got := Hello("andrkrn!", "id")
+		want := "Hai, andrkrn!"
+
+		assertCorrectMessage(t, got, want)
+	})
 }
